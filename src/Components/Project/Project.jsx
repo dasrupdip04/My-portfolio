945210540824ef234f9a0117b675b908dbb8919a
dasrupdip04 <dasrupdip04@gmail.com>
2025-04-26 19:23:28 +0530

    Added projects

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 import { ProjectDeatils } from "./ProjectDetails";
 import React from 'react';
 import YouTube from 'react-youtube';
+import { otherproj } from "./othproj";
 
 export default function Project() {
   return (
-    <section className="my-16 px-3" id="Projects">
+    <div>
+        <section className="my-16 px-3" id="Projects">
       <p className="text-3xl font-semibold text-white">Featured Projects</p>
 
       {ProjectDeatils?.map(project => (
@@ -42,5 +44,49 @@ export default function Project() {
         </article>
       ))}
     </section>
+    <section className="my-16 px-3" id="Projects">
+  
+
+  <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-4">
+    {otherproj?.map(project => (
+      <article className="group" key={project.id}>
+        <div className="flex flex-col justify-center relative">
+
+          <div className="flex flex-row peer absolute z-10 left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] duration-150 transition-opacity opacity-0 group-hover:opacity-100">
+            <Link
+              target="_blank"
+              to={project.liveLink}
+              className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-purple-600 flex items-center justify-center text-2xl font-semibold uppercase outline-none focus:outline-none mr-4 mb-1 ease-linear transition-all duration-150"
+            >
+              Live
+            </Link>
+            <Link
+              target="_blank"
+              to={project.sourceLink}
+              className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-purple-600 flex items-center justify-center text-2xl font-semibold uppercase outline-none focus:outline-none mr-4 mb-1 ease-linear transition-all duration-150"
+            >
+              Source
+            </Link>
+          </div>
+
+          <div className="peer-hover:blur-sm group-hover:blur-sm transition-all duration-300 flex flex-col space-y-3 rounded-xl shadow-lg p-3 border border-white bg-white">
+            <div className="w-full bg-white grid place-items-center h-11/12">
+              <img className="rounded-xl object-cover" src={project.img} alt={project.title} />
+            </div>
+            <div className="bg-white flex flex-col p-3">
+              <h1 className="text-gray-800 text-xl md:text-2xl font-semibold">{project.title}</h1>
+              {/* <p className="md:text-lg text-gray-500 text-base mt-2">
+                <span className="text-m font-bold text-gray-800">Tech Stack:</span> {project.techStack}
+              </p> */}
+            </div>
+          </div>
+
+        </div>
+      </article>
+    ))}
+  </div>
+</section>
+
+    </div>
   )
 }
---
+++ src/Components/Project/othproj.js
@@ -0,0 +1,73 @@
+export const otherproj=[
+    {
+      livelink: 'https://markdown-parser-rupdip.vercel.app/',
+      sourceLink: 'https://github.com/dasrupdip04/MarkdownParser',
+      id: 'Markdown Parser',
+      title: 'Markdown Parser',
+      img:'https://i.ibb.co/CKrff6tZ/Screenshot-2025-04-26-at-7-02-54-PM.png',
+      
+      techStack: 'Python, OpenAI SDK (Gemini API), OS & FileSystem Modules, JSON Handling',
+      
+      
+    }
+    ,
+    
+      {
+        livelink: 'https://currencyexchange2.vercel.app/',
+        sourceLink: 'https://github.com/dasrupdip04/currencyexchange2',
+        id: 'Injury Tracker',
+        title: 'Currency Exchange',
+        img:'https://i.ibb.co/d0xKgQgr/Screenshot-2025-04-26-at-7-12-08-PM.png',
+        
+        techStack: 'Python, OpenAI SDK (Gemini API), OS & FileSystem Modules, JSON Handling',
+        
+        
+      },
+    
+      {
+        livelink: 'https://injurytracker-rd.vercel.app/',
+        sourceLink: 'https://github.com/dasrupdip04/InjuryTracker',
+        id: 'Injury Tracker',
+        title: 'Injury Tracker',
+        img:'https://i.ibb.co/fdwtkr9F/Screenshot-2025-04-26-at-7-14-33-PM.png" alt="Screenshot-2025-04-26-at-7-14-33-PM',
+        
+        techStack: 'Python, OpenAI SDK (Gemini API), OS & FileSystem Modules, JSON Handling',
+        
+        
+      },
+      ,
+    
+      {
+        livelink: 'https://todolistrupdip.netlify.app/',
+        sourceLink: 'https://github.com/dasrupdip04/ToDoList',
+        id: 'Injury Tracker',
+        title: 'To Do App',
+        img:'https://i.ibb.co/Dgktp1NQ/Screenshot-2025-04-26-at-7-19-42-PM.png" alt="Screenshot-2025-04-26-at-7-14-33-PM',
+        
+        techStack: 'Python, OpenAI SDK (Gemini API), OS & FileSystem Modules, JSON Handling',
+        
+        
+      },
+
+      {
+        livelink: 'https://book-library-rupdip.vercel.app/',
+        sourceLink: 'https://github.com/dasrupdip04/BookLibrary',
+        id: 'Terminal Based Mini Cursor',
+        title: 'Book Library',
+        img:'https://i.ibb.co/PsrfTp91/Screenshot-2025-04-26-at-7-16-09-PM.png',
+        
+        techStack: 'Python, OpenAI SDK (Gemini API), OS & FileSystem Modules, JSON Handling',
+        
+        
+      },
+      {
+        livelink: 'https://image-curator-rd.vercel.app/',
+        sourceLink: 'https://github.com/dasrupdip04/Image-Curator-App',
+        id: 'Image Curator App',
+        title: 'Image Curator App',
+        img:'https://i.ibb.co/RTH2nVvy/Screenshot-2025-04-26-at-7-06-16-PM.png',
+        
+        techStack: 'Python, OpenAI SDK (Gemini API), OS & FileSystem Modules, JSON Handling',
+        
+        
+      },]
